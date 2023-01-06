b497e87bda2fb47bec61f03eac7d4f10d0dc7691
Alex <alexreznicek@seznam.cz>
2023-01-06 16:40:46 +0100

    first loop added

--- first.py
+++ first.py
@@ -4,3 +4,10 @@ def joe():
     print("joe mama xd")
 
 joe()
+
+for x in range (1,11):
+    if (x == 5):
+        print("Fuck this shit, I'mma not printing 5")
+        continue
+    else:
+        print(x)
