bf21692e4d177202de878e3a3eb49ff76de9cc67
Alex <alexreznicek@seznam.cz>
2023-01-12 20:45:27 +0100

    added counter with correct and wrong answers

--- svenskaapp.py
+++ svenskaapp.py
@@ -1,8 +1,5 @@
 import random
  
- #nastaveni skore
-correct = 0
-wrong = 0
 
 # Create a list of flashcards
 flashcards = [
@@ -21,18 +18,28 @@ def select_flashcard():
     return (word, translation)
 
 # Get a random flashcard and display it to the user
+correct = 0
+wrong = 0
+
 def game():
+    global correct
+    global wrong
     word, translation = select_flashcard()
     print(word)
     answer = input("What is the translation? ")
 
-    #odpoved
     if answer == translation:
         print("Correct!")
         correct +=1
     else:
         print(f"Incorrect. The correct answer is {translation}.")
         wrong +=1
+    print("")
+for x in range(0,3):
+    game()
+print(f"You had {correct} answer correct and {wrong} answers wrong")
+    
+
 
 
 
