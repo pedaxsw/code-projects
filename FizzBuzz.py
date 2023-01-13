8523e64a26528e800b386712517e151945f326c6
Alex <alexreznicek@seznam.cz>
2023-01-13 18:41:30 +0100

    project fixed and done, easy xd

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,14 +4,12 @@
 #Numbers multiples by both print FizzBuzz
 
 for x in range(1,101):
-    if x % 3 and x % 5:
+    if x % 3 ==0 and x % 5 ==0:
         print("FizzBuzz")
-        """
-    elif x % 3:
+    elif x % 3 == 0:
         print("Buzz")
-    elif x % 5:
+    elif x % 5 == 0:
         print("Fizz")
     else:
         print(x)
-        #
-        """
+        
