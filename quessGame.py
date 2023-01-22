5c378ee4aedf229114f77a0375ff6a5eece4ff97
Alex <alexreznicek@seznam.cz>
2023-01-22 16:36:18 +0100

    should work, but it doesnt

--- quessGame.py
+++ quessGame.py
@@ -1,19 +1,20 @@
 import random
 
-word = int(input("type a number from 1 - 10: "))
-while True:
-    if word < 11 and word > 0:
-        break
-    elif not word < 11 and word > 0:
-        word = int(input("Number that you have entered is not in range 1 - 10, please try again: "))
-        continue
-    else:
-           continue
+def psani(word):
+    word = int(input("type a number from 1 - 10: "))
+    while True:
+        if word < 11 and word > 0:
+            break
+        elif not (word < 11 and word > 0):
+            word = int(input("Number that you have entered is not in range 1 - 10, please try again: "))
+            continue
+    return word
 
 randomword = random.randrange(1,101)
 print(randomword)
 
 while True:
+    word = psani(word)
     if randomword == word:
         print("Correct!")
         break
