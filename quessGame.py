7788d5f7494b34fe2797f19f30f5668a13522a1f
Alex <alexreznicek@seznam.cz>
2023-01-22 16:20:33 +0100

    woeking check, but for string

--- quessGame.py
+++ quessGame.py
@@ -1,12 +1,11 @@
 import random
 
-word = input("type a number from 1 - 10: ")
+word = int(input("type a number from 1 - 10: "))
 while True:
-    if word.isdigit() and int(word) < 11 and int(word) > 0:
+    if word < 11 and word > 0:
         break
-    elif not (int(word) < 11 and int(word) >0):
-        word = input("Number that you have entered is not in range 1 - 10, please try again: ")
+    elif not word < 11 and word > 0:
+        word = int(input("Number that you have entered is not in range 1 - 10, please try again: "))
         continue
     else:
-        word = input(word, " is not a number, please try again")
-        continue
+           break
