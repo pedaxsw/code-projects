83aa8a2fe2dceaab130e8852fc1e4b0671cafd58
Alex <alexreznicek@seznam.cz>
2023-01-22 20:11:09 +0100

    Game probably done, easy way

--- rps.py
+++ rps.py
@@ -7,5 +7,26 @@ print(options[pc_choice])
 
 quess = input("Choose from Rock, Paper, Scissors: ")
 
-if quess.lower() == Rock:
+if quess.lower() == Rock and pc_choice == Scissors:
+    print("You won!")
+elif quess.lower() == Rock and pc_choice == Paper:
+    print("PC won!")
+elif quess.lower() == Rock and pc_choice == Rock:
+    print("Tie!")
 
+elif quess.lower() == Scissors and pc_choice == Scissors:
+    print("Tie!")
+elif quess.lower() == Scissors and pc_choice == Paper:
+    print("You won!")
+elif quess.lower() == Scissors and pc_choice == Rock:
+    print("Pc won!")
+
+elif quess.lower() == Paper and pc_choice == Scissors:
+    print("PC won!")
+elif quess.lower() == Paper and pc_choice == Paper:
+    print("Tie!")
+elif quess.lower() == Paper and pc_choice == Rock:
+    print("You won!")
+
+else:
+    print("Please choose from Paper, Rock or Scissors...")
