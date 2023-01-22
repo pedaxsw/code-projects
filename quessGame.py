4818a9ee0f5c780457096cafadff01dac443f6ea
Alex <alexreznicek@seznam.cz>
2023-01-22 16:52:54 +0100

    working game, just without the check

--- quessGame.py
+++ quessGame.py
@@ -1,20 +1,11 @@
 import random
 
-def psani(word):
-    word = int(input("type a number from 1 - 10: "))
-    while True:
-        if word < 11 and word > 0:
-            break
-        elif not (word < 11 and word > 0):
-            word = int(input("Number that you have entered is not in range 1 - 10, please try again: "))
-            continue
-    return word
-
 randomword = random.randrange(1,101)
 print(randomword)
 
 while True:
-    word = psani(word)
+    word = int(input("Quess your number from 1 - 100: "))
+    
     if randomword == word:
         print("Correct!")
         break
